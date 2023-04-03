cae99f011a988db5d83b87fa213761dd6797ba00
fluxfw <fluxfw@fluxlabs.ch>
2023-04-03 13:51:43 +0200

    `flux-ilias-rest-object-helper-plugin` ILIAS 8 core patch hint

--- bin/install-flux-ilias-rest-object-helper-plugin.sh
+++ bin/install-flux-ilias-rest-object-helper-plugin.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+versionIsGreaterOrEqual() {
+    if [ -n "$1" ] && [ -n "$2" ] && [ `echo -e "$2\n$1" | sort -rV | head -n 1)` = "$1" ]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+
 tag="$1"
 if [ -z "$tag" ]; then
     echo "Please pass a flux-ilias-rest-object-helper-plugin tag" >&2
@@ -12,4 +20,8 @@ fi
 
 if [ ! -d "$ILIAS_WEB_DIR/Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/flux_ilias_rest_helper_plugin" ]; then
     echo "Hint: You need to install flux-ilias-rest-helper-plugin too"
+fi
+
+if [ `versionIsGreaterOrEqual "$ilias_version_number" 8.0` = "true" ]; then
+    echo "Hint: For make this plugin work with the broken ILIAS 8 core repository plugins interface, you may need to patch the core, before you update the plugin (At your own risk)"
 fi
